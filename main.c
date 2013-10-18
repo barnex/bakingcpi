2bacf93b876a9336b5cf243127929942290f0a51
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-18 11:41:15 +0200

    cleanup

--- main.c
+++ main.c
@@ -8,10 +8,11 @@ void spin(int cycles){
 
 int main(void){
 	SetGpioFunction(16, 1);
-	while(1){
+	for(int i=0; i<10; i++){
 		SetGpio(16, 0);
-		spin(0x3F0000);
+		spin(0x300000);
 		SetGpio(16, 1);
-		spin(0x3F0000);
+		spin(0x300000);
 	}
+	return 0;
 }
