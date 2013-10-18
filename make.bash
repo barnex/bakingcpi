06195f5948bb828d64ef20c0fea77c8692e5f5af
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-18 09:28:18 +0200

    add baking pi OK03

---
+++ make.bash
@@ -0,0 +1,5 @@
+rm -f kernel.img
+./gcc/bin/arm-none-eabi-gcc -I . *.s -o kernel.o
+./gcc/bin/arm-none-eabi-ld --no-undefined kernel.o -Map kernel.map -o output.elf -T kernel.ld
+./gcc/bin/arm-none-eabi-objcopy output.elf -O binary kernel.img 
+./gcc/bin/arm-none-eabi-objdump -d output.elf > kernel.list
