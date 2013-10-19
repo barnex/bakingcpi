f9ac4201da569ee43495e15ddf62f09cb26d518b
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-19 21:21:07 +0200

    sanitize build system

--- make.bash
+++ make.bash
@@ -1,6 +1,15 @@
 rm -f kernel.img
-./gcc/bin/arm-none-eabi-gcc -O3 -nostdlib -std=c99 -I . -S *.c &&
-./gcc/bin/arm-none-eabi-as -I . *.s -o kernel.o &&
-./gcc/bin/arm-none-eabi-ld --no-undefined kernel.o -Map kernel.map -o output.elf -T kernel.ld &&
+echo gcc
+./gcc/bin/arm-none-eabi-gcc -c -I . *.c -o system.o -O3 -nostdlib -std=c99 &&
+
+echo as
+./gcc/bin/arm-none-eabi-as -I . *.s -o drivers.o &&
+
+echo ld
+./gcc/bin/arm-none-eabi-ld --no-undefined system.o drivers.o -Map kernel.map -o output.elf -T kernel.ld &&
+
+echo objcopy
 ./gcc/bin/arm-none-eabi-objcopy output.elf -O binary kernel.img  &&
+
+echo objdump
 ./gcc/bin/arm-none-eabi-objdump -d output.elf > kernel.list
